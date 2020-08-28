c9ec9d2c4b68dadc063ff15eee509ed2a3c79db4
Ricol Wang <ricol@opensimsim.com>
2020-08-29 00:05:05 +1000

    Display data in tableview.

--- Test/MainViewController.swift
+++ Test/MainViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var barItemRun: UIBarButtonItem!
+    
+    let ID = "TableViewCell_ID"
+    var data = [String]()
+    var value = [String: String]()
     
     // MARK: - View Life Cycle
     
@@ -19,6 +24,29 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        callAPI()
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let row = data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: ID, for: indexPath)
+        cell.textLabel?.text = row
+        cell.detailTextLabel?.text = value[row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return data.count
     }
     
     // MARK: - IBAction Methods
@@ -35,7 +63,9 @@ class ViewController: UIViewController
         AF.request("https://api.github.com").responseJSON { (res) in
             if let json = res.value as? [String: String]
             {
-                print(json)
+                self.data = json.keys.sorted()
+                self.value = json
+                self.tableView.reloadData()
             }
         }
     }
